7c991cdb54d6e72e1cbe093509f9c3fb364a7f68
Greg Fiumara <greg@gregfiumara.com>
2015-11-11 15:13:44 -0500

    Add content blocker target.

---
+++ ContentBlocker/ActionRequestHandler.swift
@@ -0,0 +1,23 @@
+//
+//  ActionRequestHandler.swift
+//  ContentBlocker
+//
+//  Created by Greg Fiumara on 11/11/15.
+//  Copyright © 2015 Greg Fiumara. All rights reserved.
+//
+
+import UIKit
+import MobileCoreServices
+
+class ActionRequestHandler: NSObject, NSExtensionRequestHandling {
+
+    func beginRequestWithExtensionContext(context: NSExtensionContext) {
+        let attachment = NSItemProvider(contentsOfURL: NSBundle.mainBundle().URLForResource("blockerList", withExtension: "json"))!
+    
+        let item = NSExtensionItem()
+        item.attachments = [attachment]
+    
+        context.completeRequestReturningItems([item], completionHandler: nil);
+    }
+    
+}

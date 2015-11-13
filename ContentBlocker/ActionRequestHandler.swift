d6e94b20a136527d125bd53aeda3cb18cc042f1c
Greg Fiumara <greg@gregfiumara.com>
2015-11-12 20:16:36 -0500

    Use downloaded block list when available.

--- ContentBlocker/ActionRequestHandler.swift
+++ ContentBlocker/ActionRequestHandler.swift
@@ -11,13 +11,49 @@ import MobileCoreServices
 
 class ActionRequestHandler: NSObject, NSExtensionRequestHandling {
 
-    func beginRequestWithExtensionContext(context: NSExtensionContext) {
-        let attachment = NSItemProvider(contentsOfURL: NSBundle.mainBundle().URLForResource("blockerList", withExtension: "json"))!
-    
-        let item = NSExtensionItem()
-        item.attachments = [attachment]
-    
-        context.completeRequestReturningItems([item], completionHandler: nil);
-    }
-    
+	func beginRequestWithExtensionContext(context: NSExtensionContext)
+	{
+		let fileManager = NSFileManager.defaultManager()
+		let uncoordinatedURL = fileManager.containerURLForSecurityApplicationGroupIdentifier(Constants.AppGroupID)!.URLByAppendingPathComponent(Constants.BlockListFilename)
+
+		if fileManager.fileExistsAtPath(uncoordinatedURL.path!) {
+			Logger.log("Using downloaded blocker list")
+
+			let fileCoordinator = NSFileCoordinator.init(filePresenter:nil)
+			var error:NSError?
+			var operationDidFinish:Bool = false
+			var attachment:NSItemProvider?
+			fileCoordinator.coordinateReadingItemAtURL(uncoordinatedURL, options:.ResolvesSymbolicLink, error:&error, byAccessor:{(readingURL) in
+				attachment = NSItemProvider(contentsOfURL:readingURL)
+				operationDidFinish = (attachment == nil)
+			})
+
+			if error != nil {
+				Logger.log("ERROR: \(error?.localizedDescription)")
+				context.cancelRequestWithError(error!)
+				return
+			}
+
+			if operationDidFinish == false {
+				Logger.log("ERROR: Operation did not finish")
+				context.cancelRequestWithError(NSError.init(domain:Constants.ErrorDomain, code:Constants.ErrorIO, userInfo:nil))
+				return
+			}
+
+			let item = NSExtensionItem()
+			item.attachments = [attachment!]
+			context.completeRequestReturningItems([item], completionHandler: nil);
+
+
+		} else {
+			Logger.log("Using bundled blocker list")
+
+			let attachment = NSItemProvider(contentsOfURL: NSBundle.mainBundle().URLForResource("blockerList", withExtension: "json"))!
+			let item = NSExtensionItem()
+			item.attachments = [attachment]
+			context.completeRequestReturningItems([item], completionHandler: nil);
+		}
+
+	}
+
 }

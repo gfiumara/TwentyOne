d3044df0c3ab7f43648c89ee6c9ebba9433426dd
Greg Fiumara <greg@gregfiumara.com>
2016-04-10 16:37:30 -0400

    Replace deprecated Swift file variables.

--- TwentyOne/support/Logger.swift
+++ TwentyOne/support/Logger.swift
@@ -14,7 +14,7 @@ public struct Logger
 	public static var enabled:Bool = false
 	#endif
 
-	public static func log(message:String, file:String = __FILE__, function:String = __FUNCTION__, line:Int = __LINE__)
+	public static func log(message:String, file:String = #file, function:String = #function, line:Int = #line)
 	{
 		if !Logger.enabled {
 			return
